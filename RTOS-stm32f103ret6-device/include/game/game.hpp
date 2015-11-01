ac65ba150eaec663242f15b87da30a4743393a98
Aleksey Bulatov <AlexeyABulatov@yandex.ru>
2015-11-01 13:36:25 +0300

    Sound for damage notification added

---
+++ RTOS-stm32f103ret6-device/include/game/game.hpp
@@ -0,0 +1,19 @@
+/*
+ * game.hpp
+ *
+ *  Created on: 1 нояб. 2015 г.
+ *      Author: alexey
+ */
+
+#ifndef INCLUDE_GAME_GAME_HPP_
+#define INCLUDE_GAME_GAME_HPP_
+
+
+
+
+
+
+
+
+
+#endif /* INCLUDE_GAME_GAME_HPP_ */
