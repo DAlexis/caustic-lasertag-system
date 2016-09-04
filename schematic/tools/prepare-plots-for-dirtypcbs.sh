8d6d179bdfc39c159b7779f5936770592835de8c
Aleksey Bulatov <AlexeyABulatov@yandex.ru>
2016-09-04 15:05:50 +0300

    [PCB] IR led module LW-compatible added
    [PCB] Fix for prev commit

--- schematic/tools/prepare-plots-for-dirtypcbs.sh
+++ schematic/tools/prepare-plots-for-dirtypcbs.sh
@@ -2,8 +2,11 @@
 
 (
     cd $1
-    drill=`ls | grep drl`
+    drill=`ls | grep .drl`
     txt="${drill/drl/txt}"
+    cuts_gm1=`ls | grep .gm1`
+    cuts_gbr="${cuts_gm1/gm1/gbr}"
     mv $drill $txt
+    mv $cuts_gm1 $cuts_gbr
     zip $2 *
 )
