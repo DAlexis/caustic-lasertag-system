6c6026dc5af7ed052ddbc7e2a9eb3d27b17d2b0a
Vasily Shamporov <vashamporov@mail.ru>
2017-01-15 00:44:45 +0300

    [android] Fixed back button behavior in Settings

--- Android/LasertagClientApp/app/src/main/java/ru/caustic/lasertagclientapp/SettingsActivity.java
+++ Android/LasertagClientApp/app/src/main/java/ru/caustic/lasertagclientapp/SettingsActivity.java
@@ -201,17 +201,21 @@ public class SettingsActivity extends AppCompatActivity implements colorDialog.C
         }
 
 
+    }
 
-
-        @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
-            // Handle action bar item clicks here. The action bar will
-            // automatically handle clicks on the Home/Up button, so long
-            // as you specify a parent activity in AndroidManifest.xml.
-            int id = item.getItemId();
-
-            return super.onOptionsItemSelected(item);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == android.R.id.home)
+        {
+            onBackPressed();
+            return true;
         }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
