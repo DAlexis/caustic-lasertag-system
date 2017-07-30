2ef651594c1a573e35f3d2e1db6f6c16311d2c21
Aleksey Bulatov <AlexeyABulatov@yandex.ru>
2017-07-31 01:34:20 +0300

    WIP

---
+++ Android/CausticLaserTagController/controllerapp/src/main/java/com/caustic/controllerapp/ConnectToBtActivity.java
@@ -0,0 +1,14 @@
+package com.caustic.controllerapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConnectToBtActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connect_to_bt);
+
+    }
+}
